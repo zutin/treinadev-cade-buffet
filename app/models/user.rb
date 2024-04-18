1f710c90b85ab7bcf0d9005ac694c408702389ef
zutin <gianluccaz@hotmail.com>
2024-04-18 10:32:55 -0300

    Added Devise gem, created User model and unit tests, added I18n pt-BR locale

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :full_name, :username, :contact_number, presence: true
+  # Futuramente diferenciar Cliente - Dono
+  #validates :role_id, presence: true
+  validates :username, :email, uniqueness: true
+  validates :username, length: { in: 5..20 }
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#valid?' do
+    context 'presence' do
+      it 'false when username is missing' do
+        user = User.new(username: '', full_name: 'Test User', contact_number: '(11) 99876-5432', email: 'user@test.com', password: 'password')
+        expect(user.valid?).to eq false
+      end
+
+      it 'false when full name is missing' do
+        user = User.new(username: 'usertest', full_name: '', contact_number: '(11) 99876-5432', email: 'user@test.com', password: 'password')
+        expect(user.valid?).to eq false
+      end
+
+      it 'false when contact number is missing' do
+        user = User.new(username: 'usertest', full_name: 'Test User', contact_number: '', email: 'user@test.com', password: 'password')
+        expect(user.valid?).to eq false
+      end
+
+      it 'false when email is missing' do
+        user = User.new(username: 'usertest', full_name: 'Test User', contact_number: '(11) 99876-5432', email: '', password: 'password')
+        expect(user.valid?).to eq false
+      end
+
+      it 'false when password is missing' do
+        user = User.new(username: 'usertest', full_name: 'Test User', contact_number: '(11) 99876-5432', email: 'user@test.com', password: '')
+        expect(user.valid?).to eq false
+      end
+    end
+
+    context 'uniqueness' do
+      it 'false when username is already in use' do
+        User.create!(username: 'usertest', full_name: 'Test User', contact_number: '(11) 99876-5432', email: 'user@test.com', password: 'password')
+        user = User.new(username: 'usertest', full_name: 'Non-Unique User', contact_number: '(12) 92345-6789', email: 'nonunique@user.com', password: 'uniquepassword')
+        expect(user.valid?).to eq false
+      end
+
+      it 'false when email is already in use' do
+        User.create!(username: 'usertest', full_name: 'Test User', contact_number: '(11) 99876-5432', email: 'user@test.com', password: 'password')
+        user = User.new(username: 'unique', full_name: 'Non-Unique User', contact_number: '(12) 92345-6789', email: 'user@test.com', password: 'uniquepassword')
+        expect(user.valid?).to eq false
+      end
+    end
+
+    context 'length' do
+      it 'false when username is shorter than 5 characters' do
+        user = User.new(username: 'user', full_name: 'Non-Unique User', contact_number: '(12) 92345-6789', email: 'nonunique@user.com', password: 'uniquepassword')
+        expect(user.valid?).to eq false
+      end
+
+      it 'false when password is shorter than 6 characters' do
+        user = User.new(username: 'usertest', full_name: 'Non-Unique User', contact_number: '(12) 92345-6789', email: 'nonunique@user.com', password: 'passw')
+        expect(user.valid?).to eq false
+      end
+    end
+
+    #Context pra email não valido
+  end
+end
